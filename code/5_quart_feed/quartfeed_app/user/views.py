2cb9a4d7b5b9439c2030ad3cfdb2b54c403a52ab
Jorge Escobar <jorge@fromzero.io>
2022-02-23 13:46:04 -0500

    5.3

--- code/5_quart_feed/quartfeed_app/user/views.py
+++ code/5_quart_feed/quartfeed_app/user/views.py
@@ -1,10 +1,10 @@
-from quart import Blueprint, current_app, Response
+from quart import Blueprint, current_app, render_template
 
 from user.models import user_table
 
 user_app = Blueprint("user_app", __name__)
 
 
-@user_app.route("/register")
+@user_app.route("/register", methods=["GET", "POST"])
 async def register() -> str:
-    return "<h1>User Registration</h1>"
+    return await render_template("user/register.html")
