6e4dd412c2fa55e2f95074735dff8f80552a9132
Jorge Escobar <jorge@fromzero.io>
2022-04-29 16:04:55 -0400

    5.4

--- code/5_quart_feed/quartfeed_app/user/views.py
+++ code/5_quart_feed/quartfeed_app/user/views.py
@@ -1,4 +1,4 @@
-from quart import Blueprint, current_app, render_template
+from quart import Blueprint, current_app, render_template, request
 
 from user.models import user_table
 
@@ -7,4 +7,22 @@ user_app = Blueprint("user_app", __name__)
 
 @user_app.route("/register", methods=["GET", "POST"])
 async def register() -> str:
-    return await render_template("user/register.html")
+    error: str = ""
+    username: str = ""
+    password: str = ""
+
+    if request.method == "POST":
+        form: dict = await request.form
+        username = form.get("username", "")
+        password = form.get("password", "")
+
+        if not username or not password:
+            error = "Please enter username and password"
+        else:
+            # check if the user exists
+            # register the user on the database
+            pass
+
+    return await render_template(
+        "user/register.html", error=error, username=username
+    )
