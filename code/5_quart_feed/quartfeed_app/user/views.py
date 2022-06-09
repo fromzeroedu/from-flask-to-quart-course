3c96b51e99c34f55d718fdfba7750c1a57a1405d
Jorge Escobar <jorge@fromzero.io>
2022-06-09 10:37:26 -0400

    completed csrf and user storage

--- code/5_quart_feed/quartfeed_app/user/views.py
+++ code/5_quart_feed/quartfeed_app/user/views.py
@@ -1,4 +1,6 @@
-from quart import Blueprint, current_app, render_template, request
+from quart import Blueprint, current_app, render_template, request, session
+from passlib.hash import pbkdf2_sha256
+import uuid
 
 from user.models import user_table
 
@@ -10,19 +12,40 @@ async def register() -> str:
     error: str = ""
     username: str = ""
     password: str = ""
+    csrf_token: uuid.UUID = uuid.uuid4()
+
+    if request.method == "GET":
+        session["csrf_token"] = str(csrf_token)
 
     if request.method == "POST":
         form: dict = await request.form
         username = form.get("username", "")
         password = form.get("password", "")
 
+        # check empty values
         if not username or not password:
             error = "Please enter username and password"
-        else:
-            # check if the user exists
+
+        if session.get("csrf_token") != form.get("csrf_token"):
+            error = "Invalid POST contents"
+        
+        # check if the user exists
+        conn = current_app.dbc  # type: ignore
+        query = user_table.select().where(user_table.c.username == username)
+        row = await conn.fetch_one(query=query)
+        if row:
+            error = "Username already exists"
+
+        if not error:
             # register the user on the database
-            pass
+            del session["csrf_token"]
+            hash: str = pbkdf2_sha256.hash(password)
+            stmt = user_table.insert().values(username=username, password=hash)
+            result = await conn.execute(stmt)
+            await conn.execute("commit")
+        else:
+            session["csrf_token"] = str(csrf_token)
 
     return await render_template(
-        "user/register.html", error=error, username=username
+        "user/register.html", error=error, username=username, csrf_token=csrf_token
     )
