fb2ea7a77b2bbc4b5196b77ea7e54cde9a6a0d1a
Jorge Escobar <jorge@fromzero.io>
2023-04-07 12:09:45 -0400

    mypy

--- code/5_quart_feed/quartfeed_app/user/decorators.py
+++ code/5_quart_feed/quartfeed_app/user/decorators.py
@@ -3,11 +3,11 @@ from typing import Any
 from quart import session, request, redirect, url_for
 
 
-def login_required(f):
+def login_required(f:Any) -> Any:
     @wraps(f)
-    def decorated_function(*args, **kwargs):
+    async def decorated_function(*args: Any, **kwargs: Any) -> Any:
         if session.get("username") is None:
             return redirect(url_for("user_app.login", next=request.url))
-        return f(*args, **kwargs)
+        return await f(*args, **kwargs)
     
     return decorated_function
