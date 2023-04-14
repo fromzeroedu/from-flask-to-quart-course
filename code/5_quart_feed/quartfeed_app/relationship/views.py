e1ad546154abf9cf6bd0b7a5d08bf4bb5bd2e6b2
Jorge Escobar <jorge@fromzero.io>
2023-04-14 10:22:41 -0400

    checkpoint

--- code/5_quart_feed/quartfeed_app/relationship/views.py
+++ code/5_quart_feed/quartfeed_app/relationship/views.py
@@ -3,6 +3,6 @@ from quart import Blueprint
 relationship_app = Blueprint("relationship_app", __name__)
 
 
-@relationship_app.route("/add_friend/<username>", methods=["GET"])
+@relationship_app.route("/add_friend/<username>", methods=["GET"]) 
 async def add_friend(username: str) -> str:
     return "added friend: " + username
