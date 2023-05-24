b5048e7d35edc7b343905887d80d2e165bdd3f9f
Jorge Escobar <jorge@fromzero.io>
2023-05-24 17:46:34 -0400

    type checking on models

--- code/5_quart_feed/quartfeed_app/user/models.py
+++ code/5_quart_feed/quartfeed_app/user/models.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Table, Column, Integer, String
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
-from databases.core import Database
-from databases.interfaces import Record
+if TYPE_CHECKING:
+    from databases.core import Database
+    from databases.interfaces import Record
 
 from db import metadata
 
@@ -14,7 +15,7 @@ user_table = Table(
     Column("password", String(128)),
 )
 
-async def get_user_by_username(conn: Database, username: str) -> Optional[Record]:
+async def get_user_by_username(conn: "Database", username: str) -> Optional["Record"]:
     """Get a user by username"""
     query = user_table.select().where(user_table.c.username == username)
     return await conn.fetch_one(query=query)
